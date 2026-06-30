c3877472ea0f31d6405e8eec771251d626bed707
cekadagregor <cekada.gregor@gmail.com>
2026-06-30 13:32:26 +0200

    Revert "njit to elimination"
    This reverts commit 3c02287538ba54ac6e4de5fc0dca28ced59c1648.

--- src/pbpk_lite/elimination.py
+++ src/pbpk_lite/elimination.py
@@ -1,14 +1,9 @@
 import numpy as np
-from numba import njit
-
-@njit
-def elimin_func(cl_l, cl_k, c):
-    outflow = np.zeros(16)
-    outflow[5] += cl_k * c[5]
-    outflow[6] += cl_l * c[6]
-    return outflow  
 
 def clearance_helper(cl_l, cl_k):
     def inner(c):
-        return elimin_func(cl_l, cl_k, c)
+        outflow = np.zeros(16)
+        outflow[5] += cl_k * c[5]
+        outflow[6] += cl_l * c[6]
+        return outflow
     return inner
