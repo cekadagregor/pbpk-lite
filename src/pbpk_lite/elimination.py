3c02287538ba54ac6e4de5fc0dca28ced59c1648
cekadagregor <cekada.gregor@gmail.com>
2026-06-30 13:21:35 +0200

    njit to elimination

--- src/pbpk_lite/elimination.py
+++ src/pbpk_lite/elimination.py
@@ -1,9 +1,14 @@
 import numpy as np
+from numba import njit
+
+@njit
+def elimin_func(cl_l, cl_k, c):
+    outflow = np.zeros(16)
+    outflow[5] += cl_k * c[5]
+    outflow[6] += cl_l * c[6]
+    return outflow  
 
 def clearance_helper(cl_l, cl_k):
     def inner(c):
-        outflow = np.zeros(16)
-        outflow[5] += cl_k * c[5]
-        outflow[6] += cl_l * c[6]
-        return outflow
+        return elimin_func(cl_l, cl_k, c)
     return inner
